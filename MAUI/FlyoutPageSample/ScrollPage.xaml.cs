a9c5f117c623c50dd9e8c3c7a02fc9ae20b08045
Jean-Roch Boursiquot <jean-roch.boursiquot@sdx-ag.de>
2023-06-27 14:14:54 +0200

    Initial commit

---
+++ MAUI/FlyoutPageSample/ScrollPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace FlyoutPageSample;
+
+public partial class ScrollPage : ContentPage
+{
+	public ScrollPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Xamarin/FlyoutPageNavigation/XAML/ScrollPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlyoutPageNavigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScrollPage : ContentPage
+    {
+        public ScrollPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
